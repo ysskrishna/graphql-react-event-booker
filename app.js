3ab2fa4e926748e69d4a60ff04bc391eed06a3e6
Y Siva Sai Krishna <sivasaikrishnassk@gmail.com>
2020-03-24 15:58:31 +0530

    added graphQL API and storing events

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello World');
-});
+const events = [];
+
+app.use('/graphql',graphqlHttp({
+  schema:buildSchema(`
+    type Event {
+      _id : ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!       
+    }
+
+    type RootQuery {
+      events: [Event!]!
+    }
+
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+
+    schema { 
+      query:RootQuery
+      mutation:RootMutation
+    }
+  `),
+  rootValue:{
+    events:()=>{
+      return events;
+    },
+    createEvent:(args) => {
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date().toISOString()
+      };
+      console.log(args);
+      events.push(event);
+      return event;
+    }
+  },
+  graphiql:true
+}));
 
 app.listen(PORT,()=>{
   console.log("Running express server on ", PORT);
